5e4f134d442fbee139628e33f4187033200f95b6
applleeee <97298721+applleeee@users.noreply.github.com>
2022-09-02 19:13:29 +0900

    ~#14

---
+++ src/middlewares.js
@@ -0,0 +1,34 @@
+import multer from "multer";
+
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "youtube_clone";
+  res.locals.loggedInUser = req.session.user || {};
+  next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
+
+export const avaterUpload = multer({
+  dest: "uploads/avaters",
+  limits: { fileSize: 30000000 },
+});
+
+export const videoUpload = multer({
+  dest: "uploads/videos",
+  limits: { fileSize: 100000000 },
+});
