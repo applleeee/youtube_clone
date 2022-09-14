92a0cca255b08c1f1ac8b5c2ceacf46232f478c3
applleeee <97298721+applleeee@users.noreply.github.com>
2022-09-15 03:59:01 +0900

    Delete youtube_clone directory

--- youtube_clone/src/Router/apiRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerView } from "../controller/videoController";
-
-const apiRouter = express.Router();
-
-apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
-
-export default apiRouter;
